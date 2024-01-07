a986095bd4bae2d9298ddeae143b84918c8a732d
sa1tbaby <sklitgames@gmail.com>
2024-01-07 20:31:47 +0300

    update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import datetime
 import os.path
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 import os.path
 
 cur_dir = os.path.curdir
@@ -60,13 +60,82 @@ def base():
 def product():
     return render_template('index_product.html')
 
-@app.route('/services')
-def services():
-    return render_template('services/index.html')
+
+
+
+
+@app.route('/masters/<masters_name>')
+def masters_grid(masters_name):
+
+    masters_spec = {'haircut', 'sugaring', 'coloring', 'nails', 'eyebrows'}
+
+    if masters_name in masters_spec:
+        return render_template('masters/index.html', masters_name=masters_name)
+
+
 
 @app.route('/masters')
 def masters():
-    return render_template('masters/index.html')
+    masters_name = request.values.get('masters_name')
+
+    masters_table = {
+        'name': 'some_name',
+        'title': 'some_title',
+    }
+
+    static_content = [
+        {
+            'name': 'some_name',
+            'title': 'some_title',
+            'link': 'img/masters/them1.jpg'
+        },
+        {
+            'name': 'some_name',
+            'title': 'some_title',
+            'link': 'img/masters/them1.jpg'
+        },
+        {
+            'name': 'some_name',
+            'title': 'some_title',
+            'link': 'img/masters/them1.jpg'
+        },
+    ]
+
+    return render_template('masters/index.html',
+                           masters_table=masters_table,
+                           static_content=static_content)
+
+@app.route('/services')
+def services():
+    service_name = request.values.get('service_name')
+
+
+    services_table = {
+        'name': 'some_name',
+        'title': 'some_title',
+    }
+
+    static_content = [
+        {
+            'name': 'some_name',
+            'title': 'some_title'
+        },
+        {
+            'name': 'some_name',
+            'title': 'some_title'
+        },
+        {
+            'name': 'some_name',
+            'title': 'some_title'
+        },
+    ]
+
+    return render_template('services/index.html',
+                           services_table=services_table,
+                           static_content=static_content)
+
+
+
 
 @app.route('/auth')
 def auth():
@@ -75,48 +144,79 @@ def auth():
 @app.route('/appointment',  methods=['get', 'post'])
 def appointment():
     date = datetime.date.today()
-    some_text = 'пусто'
+
     time_list = [
         {
+            "id_time":"sad1",
             "time": "09:00"
         },
         {
+            "id_time": "sad2",
             "time": "10:00"
         },
         {
+            "id_time": "sad3",
             "time": "11:00"
         },
         {
+            "id_time": "sad4",
             "time": "12:00"
         },
         {
+            "id_time": "sad5",
             "time": "13:00"
         },
         {
+            "id_time": "sad6",
             "time": "14:00"
         },
         {
+            "id_time": "sad7",
             "time": "15:00"
         },
         {
+            "id_time": "sad8",
             "time": "16:00"
         },
     ]
     service_type = 'Выберите услугу'
+    time_hidden_kostil_yopta = 'Выберите время'
+    first_name = ''
+    last_name = ''
+    phone_input = ''
+    email_input = ''
 
     if request.method == "POST":
+
         date = request.form.get('date')
         service_type = request.form.get('service_type')
-        print(request.form)
-        some_text = str(date)
+        time_hidden_kostil_yopta = request.form.get('time_hidden_kostil_yopta')
+
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+
+        phone_input = request.form.get('phone_input')
+        email_input = request.form.get('email_input')
+
         return render_template('appointment/index.html',
                                date=date,
-                               some_text=some_text,
                                time_list=time_list,
-                               service_type=service_type)
+                               service_type=service_type,
+                               time_hidden_kostil_yopta=time_hidden_kostil_yopta,
+                               last_name=last_name,
+                               first_name=first_name,
+                               phone_input=phone_input,
+                               email_input=email_input)
     else:
-        return render_template('appointment/index.html', date=date, some_text=some_text,
-                               time_list=time_list,service_type=service_type)
+        return render_template('appointment/index.html',
+                               date=date,
+                               time_list=time_list,
+                               service_type=service_type,
+                               time_hidden_kostil_yopta=time_hidden_kostil_yopta,
+                               last_name=last_name,
+                               first_name=first_name,
+                               phone_input=phone_input,
+                               email_input=email_input)
 
 
 
@@ -139,5 +239,6 @@ def dev():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
---
+++ tesss.py
@@ -0,0 +1,3 @@
+masters_spec = tuple(['Брови','Ногти','Окрашивание','Шугаринг','Стрижка'])
+asd = set(1,2,3,4)
+print(masters_spec)
