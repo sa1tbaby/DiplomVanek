05522e0a0ec706d61107fd529dbb6cc8c7fd7ee9
sa1tbaby <sklitgames@gmail.com>
2024-01-09 02:43:51 +0300

    add data and some part of db

---
+++ app/models/declarativeModels.py
@@ -0,0 +1,33 @@
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import (DECIMAL,
+                        JSON,
+                        String)
+from decimal import Decimal
+from typing import Annotated, Optional
+from pydantic import BaseModel
+
+
+class ConfigDB(BaseModel):
+    dialect: str = 'postgresql'
+    driver: str = 'psycopg2'
+    username: str = 'cesium'
+    password: str = 'user'
+    host: str = '192.168.1.148'
+    port: str = '5432'
+    database: str
+    engine_echo: bool = True
+    engine_pool_size: int = 5
+
+intpk = Annotated[int, mapped_column(primary_key=True)]
+
+class BaseBd(DeclarativeBase):
+    pass
+class Users(BaseBd):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    email: Mapped[str]
+    phone_number: Mapped[str] = mapped_column(String(20))
+    role: Mapped[str]
--- tesss.py
+++ tesss.py
@@ -1,3 +1,80 @@
+import datetime
+import time
 masters_spec = tuple(['Брови','Ногти','Окрашивание','Шугаринг','Стрижка'])
-asd = set(1,2,3,4)
-print(masters_spec)
+
+import json
+
+smena = 3
+graphic = str()
+for i in range(2, 32):
+    d, v = divmod(i,smena)
+    if v != 0:
+        graphic+='1'
+    else:
+        graphic+='0'
+
+print(graphic)
+
+ww = datetime.datetime(2024, 1, 10, 16, 30)
+www = "2024-01-10 16:30:00"
+print(type(datetime.datetime.fromisoformat(www)))
+print(type(ww))
+
+with open('ser.txt', 'r', encoding='UTF-8') as file:
+    data = file.read().split('\n')
+print(data)
+
+name=''
+type_=''
+cost=''
+listing = list()
+
+name_f = name.find('.')
+name_s = name.find('-')
+for i in data:
+
+    tmp_dict = {}
+
+    if '.' not in i:
+        type_ = i
+        continue
+
+    name = i
+    cost = i
+
+    name_f = name.find('.') + 2
+    name_s = name.find('-') - 1
+
+    cost_f = cost.find('-') + 2
+    cost_s = cost.rfind('р')
+
+    name = name[name_f:name_s]
+    cost = cost[cost_f:cost_s]
+
+
+    tmp_dict['type'] = type_
+    tmp_dict['name'] = name
+    tmp_dict['cost'] = cost
+    tmp_dict['content_id'] = ''
+
+    listing.append(tmp_dict)
+
+
+with open('static/data/services.json', 'r') as file:
+    asd = json.loads(file.read())
+
+asd = asd.get('data')
+
+for j, i in enumerate(asd):
+    print(j, i.get('type'))
+
+"""
+
+with open('static/data/services.json', 'w') as file:
+    tmp = {
+        'data': listing
+    }
+    json.dump(tmp, file)
+
+
+"""
