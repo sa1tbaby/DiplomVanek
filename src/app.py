91f0b4ce6a9cf5b44a25ae641a704881493e67d0
pentelejchuk.er <sklitgames@gmail.com>
2024-03-07 17:56:58 +0300

    refactoring, add new template for ui, add new handlers for backend

--- src/app.py
+++ src/app.py
@@ -1,28 +1,83 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
 
-    return render_template('user/main.html')
+    static_content = {
+        "carousel": [],
+        "carousel_active": "",
+        "information_block": []
+    }
+
+    return render_template('ui/main.html', static_content=static_content)
 
 @app.route('/services')
 def services():
 
-    return render_template('user/services.html')
+    static_content = {
+        "header": "",
+        "information": "",
+        "services_list": []
+    }
+
+    return render_template('ui/services.html', static_content=static_content)
 
 @app.route('/masters')
 def masters():
 
-    return render_template('user/masters.html')
+    static_content = {
+        "header": "",
+        "information": "",
+        "masters_list": []
+    }
 
-@app.route('/appointment')
-def appointment():
+    return render_template('ui/masters.html', static_content=static_content)
 
-    return render_template('user/appointment.html')
+
+@app.route('/appointment/<int:step>', methods=['get', 'post'])
+def appointment(step: int):
+
+    if request.method == "GET":
+
+        static_content = {
+            "services_list": []
+        }
+
+        return render_template('ui/appointment/step1.html', static_content=static_content)
+
+    elif request.method == "POST":
+
+        match step:
+
+            case 2:
+                client_request = {
+                    "service_type": request.form.get('service_type'),
+                    "date_input": request.form.get('date_input'),
+                }
+
+                static_content = {
+                    "masters_list": {}
+                }
+
+                return render_template('ui/appointment/step1.html', static_content=static_content)
+
+            case 3:
+                static_content = {
+
+                }
+
+                return render_template('ui/appointment/step1.html', static_content=static_content)
+
 
 @app.route('/base')
 def base():
+
+    static_content = {
+        "carousel": [],
+        "carousel_active": [],
+        "information_block": []
+    }
 
     return render_template('base.html')
